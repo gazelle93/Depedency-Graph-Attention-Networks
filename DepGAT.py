0a17afec4d4172cf7be7c787cc667927494ca0d1
Mingyou Sung <mingyousung@gmail.com>
2022-04-14 10:02:48 -0400

    Update DepGAT.py

--- DepGAT.py
+++ DepGAT.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class Dependency_GAT(nn.Module):
     def __init__(self, in_dim, out_dim, alpha, dependency_list):
         super(Dependency_GAT, self).__init__()
